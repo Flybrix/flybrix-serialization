7eaea3b9c3743eba2466ebce69eafd438a89396f
Adnan Ademovic <adnanademovic100@gmail.com>
2018-04-27 22:11:28 +0200

    Add parser

---
+++ src/parser.js
@@ -0,0 +1,400 @@
+(function (global) {
+    'use strict';
+
+    function StringToken(position, value) {
+        this.position = position;
+        this.value = value;
+    }
+
+    var numericTest = /^\d+$/;
+    var nameTest = /^\w+$/;
+
+    var TokenCategories = {
+        SYMBOL: 0,
+        NUMBER: 1,
+        NAME: 2,
+    };
+
+    function Token(stringToken) {
+        this.position = stringToken.position;
+        this.value = stringToken.value;
+        if (numericTest.test(this.value)) {
+            this.category = TokenCategories.NUMBER;
+            this.value = parseInt(this.value);
+        } else if (nameTest.test(this.value)) {
+            this.category = TokenCategories.NAME;
+        } else {
+            this.category = TokenCategories.SYMBOL;
+        }
+    }
+
+    var validCharSetTest = /^[{}\[\]()\/=:,;\w\s]*$/;
+
+    var isValid = function (text) {
+        return validCharSetTest.test(text);
+    };
+
+    var tokenizer = function (text) {
+        if (!isValid(text)) {
+            throw new Error('Passed config contains invalid characters');
+        }
+        var re = /([{}\[\]()\/=:,;]|\w+)/g;
+        var match;
+        var matches = [];
+        while ((match = re.exec(text)) !== null) {
+            matches.push(new StringToken(match.index, match[0]));
+        }
+        return matches;
+    };
+
+    var lexer = function (tokens) {
+        return tokens.map(function (token) {
+            return new Token(token);
+        });
+    };
+
+    var TypeCategories = {
+        NAMED: 0,
+        MAP_UNMASKED: 2,
+        MAP_MASKED: 3,
+        TUPLE_UNMASKED: 4,
+        TUPLE_MASKED: 5,
+        ARRAY_UNMASKED: 6,
+        ARRAY_MASKED: 7,
+    };
+
+    function Type(category, properties, mask) {
+        this.category = category;
+        this.properties = properties;
+        this.mask = mask || 0;
+    }
+
+    Type.prototype.generateHandler = function (library) {
+        var handlers = global.FlybrixSerialization._handlers;
+        var props = this.properties;
+        var mask = this.mask;
+        var handler = null;
+        var children;
+        switch (this.category) {
+            case TypeCategories.NAMED:
+                if (props[0] === 's') {
+                    var length = props.substring(1);
+                    if (numericTest.test(length)) {
+                        handler = handlers.string(parseInt(length));
+                    }
+                } else if (props in handlers) {
+                    handler = handlers[props];
+                    if (!handler.isBasic) {
+                        handler = null;
+                    }
+                } else if (props in library) {
+                    handler = library[props];
+                }
+                if (!handler) {
+                    throw {
+                        position: -1,
+                        error: 'Unrecognized type "' + props + '"',
+                    };
+                }
+                return handler;
+            case TypeCategories.MAP_UNMASKED:
+                children = props.map(function (child) {
+                    return {
+                        key: child.name,
+                        handler: child.value.generateHandler(library),
+                    };
+                });
+                return handlers.mapUnmasked(children);
+            case TypeCategories.MAP_MASKED:
+                children = props.map(function (child) {
+                    return {
+                        key: child.name,
+                        handler: child.value.generateHandler(library),
+                    };
+                });
+                return handlers.mapMasked(children, mask);
+            case TypeCategories.TUPLE_UNMASKED:
+                children = props.map(function (child) {
+                    return child.generateHandler(library);
+                });
+                return handlers.tupleUnmasked(children);
+            case TypeCategories.TUPLE_MASKED:
+                children = props.map(function (child) {
+                    return child.generateHandler(library);
+                });
+                return handlers.tupleMasked(children, mask);
+            case TypeCategories.ARRAY_UNMASKED:
+                return handlers.arrayUnmasked(props.count, props.value.generateHandler(library));
+            case TypeCategories.ARRAY_MASKED:
+                return handlers.arrayMasked(props.count, props.value.generateHandler(library), mask);
+            default:
+                throw {
+                    position: -1,
+                    error: 'Unrecognized type category',
+                };
+        }
+    };
+
+    var readToken = function (serializer) {
+        var token = serializer.dataView[serializer.index];
+        serializer.add(1);
+        if (!token) {
+            throw {
+                position: -1,
+                error: 'Unexpected end of string',
+            };
+        }
+        return token;
+    };
+
+    var nameParser = function (serializer) {
+        var token = readToken(serializer);
+        if (token.category !== TokenCategories.NAME) {
+            throw {
+                position: token.position,
+                error: 'Expected name, got: "' + token.value + '"',
+            };
+        }
+        if (token.value === 'MASK') {
+            throw {
+                position: token.position,
+                error: 'Disallowed name "MASK" given',
+            };
+        }
+        return token.value;
+    };
+
+    var numberParser = function (serializer) {
+        var token = readToken(serializer);
+        if (token.category !== TokenCategories.NUMBER) {
+            throw {
+                position: token.position,
+                error: 'Expected number, got: "' + token.value + '"',
+            };
+        }
+        return token.value;
+    };
+
+    var consumeSymbol = function (serializer, symbol) {
+        var token = readToken(serializer);
+        if (token.value !== symbol) {
+            throw {
+                position: token.position,
+                error: 'Expected "' + symbol + '", got: "' + token.value + '"',
+            };
+        }
+    };
+
+    var maskParser = function (serializer) {
+        // "//" or "/<NUMBER>/", otherwise there is no mask
+        // Labeled with <MASK> in comments below
+        var token = readToken(serializer);
+        if (token.value !== '/') {
+            serializer.add(-1);
+            return {
+                masked: false,
+            };
+        }
+        token = readToken(serializer);
+        if (token.value === '/') {
+            return {
+                masked: true,
+                defined: false,
+            };
+        }
+        if (token.category !== TokenCategories.NUMBER) {
+            throw {
+                position: token.position,
+                error: 'Expected "/" or number',
+            };
+        }
+        var value = token.value;
+        token = readToken(serializer);
+        if (token.value !== '/') {
+            throw {
+                position: token.position,
+                error: 'Expected "/"',
+            };
+        }
+        return {
+            masked: true,
+            defined: true,
+            value: value,
+        };
+    };
+
+    var typeMapParser = function (serializer) {
+        // {<MASK> <NAME>:<TYPE>, <NAME>:<TYPE>, <NAME>:<TYPE>}
+        var mask = maskParser(serializer);
+        var children = [];
+        while (true) {
+            var name = nameParser(serializer);
+            consumeSymbol(serializer, ':');
+            var value = typeParser(serializer);
+            children.push({
+                name: name,
+                value: value,
+            });
+            var token = readToken(serializer);
+            if (token.value === '}') {
+                if (mask.masked) {
+                    if (mask.defined) {
+                        return new Type(TypeCategories.MAP_MASKED, children, mask.value);
+                    } else {
+                        return new Type(TypeCategories.MAP_MASKED, children);
+                    }
+                } else {
+                    return new Type(TypeCategories.MAP_UNMASKED, children);
+                }
+            }
+            if (token.value !== ',') {
+                throw {
+                    position: token.position,
+                    error: 'Unexpected token after map element: "' + token.value + '"',
+                };
+            }
+        }
+    };
+
+    var typeTupleParser = function (serializer) {
+        // (<MASK> <TYPE>, <TYPE>, <TYPE>)
+        var mask = maskParser(serializer);
+        var children = [];
+        while (true) {
+            children.push(typeParser(serializer));
+            var token = readToken(serializer);
+            if (token.value === ')') {
+                if (mask.masked) {
+                    if (mask.defined) {
+                        return new Type(TypeCategories.TUPLE_MASKED, children, mask.value);
+                    } else {
+                        return new Type(TypeCategories.TUPLE_MASKED, children);
+                    }
+                } else {
+                    return new Type(TypeCategories.TUPLE_UNMASKED, children);
+                }
+            }
+            if (token.value !== ',') {
+                throw {
+                    position: token.position,
+                    error: 'Unexpected token after tuple element: "' + token.value + '"',
+                };
+            }
+        }
+    };
+
+    var typeArrayParser = function (serializer) {
+        // [<MASK> <TYPE>:<NUMBER>]
+        var mask = maskParser(serializer);
+        var value = typeParser(serializer);
+        consumeSymbol(serializer, ':');
+        var count = numberParser(serializer);
+        consumeSymbol(serializer, ']');
+        var children = {
+            value: value,
+            count: count,
+        };
+        if (mask.masked) {
+            if (mask.defined) {
+                return new Type(TypeCategories.ARRAY_MASKED, children, mask.value);
+            } else {
+                return new Type(TypeCategories.ARRAY_MASKED, children);
+            }
+        } else {
+            return new Type(TypeCategories.ARRAY_UNMASKED, children);
+        }
+    };
+
+    var typeParser = function (serializer) {
+        // Options:
+        // - <NAME>
+        // - Tuple
+        // - Array
+        // - Map
+        var token = readToken(serializer);
+        if (!token) {
+            throw {
+                position: -1,
+                error: 'Unexpected end of string',
+            };
+        }
+        if (token.category === TokenCategories.NUMBER) {
+            throw {
+                position: token.position,
+                error: 'Unexpected number, type expected',
+            };
+        }
+        if (token.category === TokenCategories.NAME) {
+            return new Type(TypeCategories.NAMED, token.value);
+        }
+        if (token.value === '{') {
+            return typeMapParser(serializer);
+        }
+        if (token.value === '[') {
+            return typeArrayParser(serializer);
+        }
+        if (token.value === '(') {
+            return typeTupleParser(serializer);
+        }
+        throw {
+            position: token.position,
+            error: 'Unexpected token when describing type: "' + token.value + '"',
+        };
+    };
+
+    var parser = function (tokens, source) {
+        var serializer = new global.FlybrixSerialization.Serializer(tokens);
+        var structures = [];
+        while (serializer.index < serializer.dataView.length) {
+            var name = nameParser(serializer);
+            if (name[0] !== name[0].toUpperCase()) {
+                throw {
+                    position: -1,
+                    error: 'Structure names cannot start with lowercase letters',
+                };
+            }
+            consumeSymbol(serializer, '=');
+            var value = typeParser(serializer);
+            consumeSymbol(serializer, ';');
+            structures.push({
+                name: name,
+                value: value,
+            });
+        }
+        return structures;
+    };
+
+    var createHandlers = function (structure) {
+        var library = {};
+        structure.forEach(function (entry) {
+            library[entry.name] = entry.value.generateHandler(library);
+        });
+        return library;
+    };
+
+    var parse = function (text) {
+        try {
+            return createHandlers(parser(lexer(tokenizer(text), text), text));
+        } catch (err) {
+            throw new Error(err.error);
+        }
+    };
+
+    if (!global.FlybrixSerialization) {
+        global.FlybrixSerialization = {};
+    }
+
+    global.FlybrixSerialization._parserSteps = {
+        tokenizer: tokenizer,
+        lexer: lexer,
+        parser: parser,
+        TokenCategories: TokenCategories,
+        TypeCategories: TypeCategories,
+        StringToken: StringToken,
+        Token: Token,
+        Type: Type,
+    };
+
+    global.FlybrixSerialization.parse = parse;
+
+}(this));
---
+++ test/parser.js
@@ -0,0 +1,242 @@
+describe('Parser', function () {
+    beforeEach(function () {
+        var steps = FlybrixSerialization._parserSteps;
+        this.tokenizer = steps.tokenizer;
+        this.lexer = steps.lexer;
+        this.parser = steps.parser;
+        this.StringToken = steps.StringToken;
+        this.Token = steps.Token;
+        this.TokenCategories = steps.TokenCategories;
+        this.parse = FlybrixSerialization.parse;
+    });
+
+    describe('tokenizer', function () {
+        it('exists', function () {
+            expect(this.tokenizer).toBeDefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof this.tokenizer).toBe('function');
+        });
+
+        it('rejects invalid characters', function () {
+            var tokenizer = this.tokenizer;
+
+            expect(function () {
+                tokenizer('Foo = { bar: u8, baz: v7[44}]/\n\r\t//()::;,');
+            }).not.toThrow();
+
+            expect(function () {
+                tokenizer('\\$%^');
+            }).toThrow();
+        });
+
+        it('splits into relevant symbols and strings', function () {
+            var tokens = this.tokenizer('Foo = { bar: u8, baz: v7[44}]/\n\r\t//()::;,');
+
+            expect(tokens.map(function (v) {
+                return v.value;
+            })).toEqual(
+                ['Foo', '=', '{', 'bar', ':', 'u8', ',', 'baz', ':', 'v7', '[',
+                    '44', '}', ']', '/', '/', '/', '(', ')', ':', ':', ';', ',']
+            );
+
+            expect(tokens.map(function (v) {
+                return v.position;
+            })).toEqual(
+                [0, 4, 6, 8, 11, 13, 15, 17, 20, 22, 24,
+                    25, 27, 28, 29, 33, 34, 35, 36, 37, 38, 39, 40]
+            );
+        });
+    });
+
+    describe('lexer', function () {
+        it('exists', function () {
+            expect(this.lexer).toBeDefined();
+        });
+
+        it('is a function', function () {
+            expect(typeof this.lexer).toBe('function');
+        });
+
+        it('splits into relevant symbols and strings', function () {
+            var StringToken = this.StringToken;
+            var name = this.TokenCategories.NAME;
+            var num = this.TokenCategories.NUMBER;
+            var sym = this.TokenCategories.SYMBOL;
+            var tokens = this.lexer([
+                new StringToken(0, 'Foo'),
+                new StringToken(4, '='),
+                new StringToken(6, '{'),
+                new StringToken(8, 'bar'),
+                new StringToken(11, ':'),
+                new StringToken(13, 'u8'),
+                new StringToken(15, ','),
+                new StringToken(17, 'baz'),
+                new StringToken(20, ':'),
+                new StringToken(22, 'v7'),
+                new StringToken(24, '['),
+                new StringToken(25, '44'),
+                new StringToken(27, '}'),
+                new StringToken(28, ']'),
+                new StringToken(29, '/'),
+                new StringToken(33, '/'),
+                new StringToken(34, '/'),
+                new StringToken(35, '('),
+                new StringToken(36, ')'),
+                new StringToken(37, ':'),
+                new StringToken(38, ':'),
+                new StringToken(39, ';'),
+                new StringToken(40, ','),
+            ]);
+
+            expect(tokens.map(function (v) {
+                return v.value;
+            })).toEqual(
+                ['Foo', '=', '{', 'bar', ':', 'u8', ',', 'baz', ':', 'v7', '[',
+                    44, '}', ']', '/', '/', '/', '(', ')', ':', ':', ';', ',']
+            );
+
+            expect(tokens.map(function (v) {
+                return v.position;
+            })).toEqual(
+                [0, 4, 6, 8, 11, 13, 15, 17, 20, 22, 24,
+                    25, 27, 28, 29, 33, 34, 35, 36, 37, 38, 39, 40]
+            );
+
+            expect(tokens.map(function (v) {
+                return v.category;
+            })).toEqual(
+                [name, sym, sym, name, sym, name, sym, name, sym, name, sym,
+                    num, sym, sym, sym, sym, sym, sym, sym, sym, sym, sym, sym]
+            );
+        });
+    });
+
+    describe('parse', function () {
+        it('handles one name alias', function () {
+            var handlers = this.parse('V=u8;');
+            expect(Object.keys(handlers).sort()).toEqual(['V']);
+            expect(handlers.V.descriptor).toBe('u8');
+        });
+
+        it('handles multiple name aliases', function () {
+            var handlers = this.parse(
+                'Vu8=u8;' +
+                'Vu16=u16;' +
+                'Vu32=u32;' +
+                'Vi8=i8;' +
+                'Vi16=i16;' +
+                'Vi32=i32;' +
+                'Vf32=f32;' +
+                'Vf64=f64;' +
+                'Vs12=s12;' +
+                'Vs3=s3;' +
+                'Vindirect=Vs12;' +
+                'Vindirecter=Vindirect;');
+            expect(Object.keys(handlers).sort()).toEqual([
+                'Vf32', 'Vf64',
+                'Vi16', 'Vi32', 'Vi8',
+                'Vindirect', 'Vindirecter',
+                'Vs12', 'Vs3',
+                'Vu16', 'Vu32', 'Vu8']);
+            expect(handlers.Vu8.descriptor).toBe('u8');
+            expect(handlers.Vu16.descriptor).toBe('u16');
+            expect(handlers.Vu32.descriptor).toBe('u32');
+            expect(handlers.Vi8.descriptor).toBe('i8');
+            expect(handlers.Vi16.descriptor).toBe('i16');
+            expect(handlers.Vi32.descriptor).toBe('i32');
+            expect(handlers.Vf32.descriptor).toBe('f32');
+            expect(handlers.Vf64.descriptor).toBe('f64');
+            expect(handlers.Vs12.descriptor).toBe('s12');
+            expect(handlers.Vs3.descriptor).toBe('s3');
+            expect(handlers.Vindirect.descriptor).toBe('s12');
+            expect(handlers.Vindirecter.descriptor).toBe('s12');
+        });
+
+        it('handles arrays', function () {
+            expect(this.parse('V=[u8:12];').V.descriptor).toBe('[u8:12]');
+        });
+
+        it('handles masked arrays', function () {
+            expect(this.parse('V=[//u8:12];').V.descriptor).toBe('[/16/u8:12]');
+            expect(this.parse('V=[/20/u8:12];').V.descriptor).toBe('[/24/u8:12]');
+        });
+
+        it('handles tuples', function () {
+            expect(this.parse('V=(u8,i16,s4);').V.descriptor).toBe('(u8,i16,s4)');
+        });
+
+        it('handles masked tuples', function () {
+            expect(this.parse('V=(//u8,i16,s4);').V.descriptor).toBe('(/8/u8,i16,s4)');
+            expect(this.parse('V=(/20/u8,i16,s4);').V.descriptor).toBe('(/24/u8,i16,s4)');
+        });
+
+        it('handles structures', function () {
+            expect(this.parse('V={a:u8,b:u16,c:s32};').V.descriptor).toBe('{a:u8,b:u16,c:s32}');
+        });
+
+        it('handles masked structures', function () {
+            expect(this.parse('V={//a:u8,b:u16,c:s32};').V.descriptor).toBe('{/8/a:u8,b:u16,c:s32}');
+            expect(this.parse('V={/10/a:u8,b:u16,c:s32};').V.descriptor).toBe('{/16/a:u8,b:u16,c:s32}');
+        });
+
+        it('handles a config', function () {
+            var text = '' +
+                'Version = {' +
+                '  major: u8,' +
+                '  minor: u8,' +
+                '  patch: u8' +
+                '};' +
+                'Vector3 = {' +
+                '  x: f32,' +
+                '  y: f32,' +
+                '  z: f32' +
+                '};' +
+                'Color = {' +
+                '  red: u8,' +
+                '  green: u8,' +
+                '  blue: u8' +
+                '};' +
+                'Led = {' +
+                '  color1: Color,' +
+                '  color2: Color,' +
+                '  pattern: u8' +
+                '};' +
+                'Configuration = {/16/' +
+                '  version: Version,' +
+                '  magnetometerBias: Vector3,' +
+                '  ledPatterns: [// Led : 16]' +
+                '};' +
+                'Commands = {/32/' +
+                '  setConfig: Configuration,' +
+                '  getConfig: {' +
+                '    mainMask: u16,' +
+                '    ledMask: u16' +
+                '  }' +
+                '};' +
+                'Response = {' +
+                '  requests: u32,' +
+                '  successful: u32' +
+                '};' +
+                '';
+            var handlers = this.parse(text);
+            expect(Object.keys(handlers).sort()).toEqual([
+                'Color',
+                'Commands',
+                'Configuration',
+                'Led',
+                'Response',
+                'Vector3',
+                'Version',
+            ]);
+            expect(handlers.Color.descriptor).toBe('{red:u8,green:u8,blue:u8}');
+            expect(handlers.Version.descriptor).toBe('{major:u8,minor:u8,patch:u8}');
+            expect(handlers.Vector3.descriptor).toBe('{x:f32,y:f32,z:f32}');
+            expect(handlers.Response.descriptor).toBe('{requests:u32,successful:u32}');
+            expect(handlers.Led.descriptor).toBe('{color1:{red:u8,green:u8,blue:u8},color2:{red:u8,green:u8,blue:u8},pattern:u8}');
+            expect(handlers.Configuration.descriptor).toBe('{/16/version:{major:u8,minor:u8,patch:u8},magnetometerBias:{x:f32,y:f32,z:f32},ledPatterns:[/16/{color1:{red:u8,green:u8,blue:u8},color2:{red:u8,green:u8,blue:u8},pattern:u8}:16]}');
+            expect(handlers.Commands.descriptor).toBe('{/32/setConfig:{/16/version:{major:u8,minor:u8,patch:u8},magnetometerBias:{x:f32,y:f32,z:f32},ledPatterns:[/16/{color1:{red:u8,green:u8,blue:u8},color2:{red:u8,green:u8,blue:u8},pattern:u8}:16]},getConfig:{mainMask:u16,ledMask:u16}}');
+        });
+    });
+});
